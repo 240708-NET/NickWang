a237b5349ff7b409df6da43263a955ece8a57bf7
nick wang <nickwang11235@gmail.com>
2024-07-11 07:54:12 -0700

    commit to keep up to date

--- app/Program.cs
+++ app/Program.cs
@@ -1,29 +1,17 @@
-﻿class Program{
-    public static void Main(string[] args){
-        Console.WriteLine("BEGIN TEXT:");
-        int target = 0;
-        int guess = 0;
-        int count = 0;
+﻿using System.Text;
 
-        Random rand = new Random();
-        target = rand.Next(1000);
-        Console.WriteLine(target);
-
-        while(true){
-            Console.WriteLine("PROMPT TEXT:");
-            if(!Int32.TryParse(Console.ReadLine(), out guess)){
-                continue;
-            }
-            count++;
-            if(target == guess){
-                Console.WriteLine("Right guess");
-                break;
-            }
-            if(target > guess){
-                Console.WriteLine("Too Small");
-            }else{
-                Console.WriteLine("Too Large");
-            }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int N = 4;
+        StringBuilder sb = new StringBuilder();
+        var temp = Enumerable.Range(1, 10);
+        foreach (var i in temp)
+        {
+            
         }
+
+        Console.WriteLine(temp.GetType());
     }
 }
